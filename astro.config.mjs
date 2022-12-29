09bc1c6edcd35fbcc515afe9e3cd04261df5f480
Jacob Ford <j@cobford.com>
2022-12-29 12:44:32 -0500

    Hacky TOC

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  markdown: {
+    extendPlugins: 'astroDefaults',
+    rehypePlugins: [
+      'rehype-slug',
+      ['rehype-toc', {
+        headings: ['h2', 'h3'],
+        cssClasses: {
+          toc: 'course-toc'
+        }
+      }],
+    ]
+  }
+})
