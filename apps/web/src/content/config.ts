c3793e436e214c4608a1164347764b840037d522
Jacob Ford <j@cobford.com>
2023-12-20 21:02:43 -0500

    Define content schema

--- apps/web/src/content/config.ts
+++ apps/web/src/content/config.ts
@@ -1,6 +1,21 @@
 import { defineCollection } from 'astro:content'
 import { docsSchema } from '@astrojs/starlight/schema'
 
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const coursesCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      intro: z.string(),
+      partnerName: z.string(),
+      partnerUrl: z.string().url(),
+      duration: z.string(), // TODO: number
+    })
+})
+
+// Export a single `collections` object to register your collection(s)
 export const collections = {
-  docs: defineCollection({ schema: docsSchema() }),
+  courses: coursesCollection,
 }
