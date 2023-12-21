657ee95d26e0517725c35ac2eb55f7b8b5359d4f
Jacob Ford <j@cobford.com>
2023-12-21 10:11:48 -0500

    Move ABCD into directory for splitting

--- apps/starlight/astro.config.mjs
+++ apps/starlight/astro.config.mjs
@@ -11,8 +11,24 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: 'Courses',
-					autogenerate: { directory: 'courses' },
+					label: 'Storytelling',
+					items: [
+						{ label: 'Public Narrative', link: 'https://learning.weavers.org/storytelling/public-narrative' },
+						{ label: 'Public Speaking', link: 'https://learning.weavers.org/storytelling/public-speaking' },
+					],
+				},
+				{
+					label: 'Gathering',
+					items: [
+						{ label: 'Facilitating Groups', link: 'https://learning.weavers.org/gathering/group-facilitation/' },
+						{ label: 'Facilitating Circles', link: 'https://learning.weavers.org/gathering/circle-facilitation' },
+					],
+				},
+				{
+					label: 'Action',
+					items: [
+						{ label: 'Asset-Based Community Development', link: '/courses/asset-based-community-development/' },
+					],
 				},
 			],
 		}),
