7c65b7130497635287be76633086e35c3b76c952
Jacob Ford <j@cobford.com>
2023-12-29 10:06:41 -0500

    Copy markdown config from astro/docs

--- apps/starlight/astro.config.mjs
+++ apps/starlight/astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import rehypeSlug from 'rehype-slug';
+import remarkSmartypants from 'remark-smartypants';
+
+import { rehypeTasklistEnhancer } from './plugins/rehype-tasklist-enhancer';
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
@@ -11,7 +16,7 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: 'Courses',
+					label: 'All Courses',
 					autogenerate: { directory: 'courses' },
 				}
 			],
@@ -20,4 +25,17 @@ export default defineConfig({
 			],
 		}),
 	],
+	trailingSlash: 'always',
+	markdown: {
+		// Override with our own config
+		smartypants: false,
+		remarkPlugins: [
+			remarkSmartypants,
+		],
+		rehypePlugins: [
+			rehypeSlug,
+			// Tweak GFM task list syntax
+			rehypeTasklistEnhancer(),
+		],
+	},
 });
