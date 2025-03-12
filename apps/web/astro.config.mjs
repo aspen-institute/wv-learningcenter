6e0b854852f25f8ca64cde7c84f93949f3df3b21
Jacob Ford <j@cobford.com>
2025-03-12 12:05:25 -0400

    Fix iOS auto smart quotes

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -66,13 +66,13 @@ export default defineConfig({
 				// 	autogenerate: { directory: 'courses/public-speaking' },
 				// },
 				{
-					label: ‘Types of Gatherings’,
+					label: 'Types of Gatherings',
 					collapsed: true,
 					badge: {
-						text: ‘Gatherings’,
-						variant: ‘danger’
+						text: 'Gatherings',
+						variant: 'danger',
 					},
-					autogenerate: { directory: ‘courses/types-of-gatherings’ },
+					autogenerate: { directory: 'courses/types-of-gatherings' },
 				},
 				{
 					label: 'Facilitating Circles',
